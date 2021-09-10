fd5b584d836b150345e678dc68b52a50c9aaff89
JulianeGF <juliane.ferreira@elo7.com>
2021-09-10 09:02:08 -0300

    wip

---
+++ PDI/PDI/RequestHelper.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+
+
+//app-platform = android ou ios;
+//app-version = 3.0.0 <- a gente deixa esse valor pros testes que a gente faz;
+//app-id = elo7
+
+//NSDictionary *cookieProperties = [self cookiePropertiesWithKey:@"app-platform" andValue:@"ios" forPath:@"/" forDomain:@"www.elo7.com.br"];
+//NSHTTPCookie *cookie = [NSHTTPCookie cookieWithProperties:cookieProperties];
+//[[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:cookie];
